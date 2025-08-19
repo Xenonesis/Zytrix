d42b74836dc21983a3c53c86f02eb9d5719a9f64
xenonesis <aaktaditya09@gmail.com>
2025-08-19 18:31:51 +0530

    Add File polyfill to entry.client.tsx to fix ReferenceError in Netlify builds

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,3 +1,20 @@
+/* Polyfill for File API in environments where it's not available */
+// @ts-ignore: Global is not defined in browser environment
+if (typeof File === 'undefined') {
+  // @ts-ignore: Global is not defined in browser environment
+  global.File = class File extends Blob {
+    name: string;
+    lastModified: number;
+
+    constructor(chunks: any[], filename: string, options?: any) {
+      // @ts-ignore: Super constructor call
+      super(chunks, options);
+      this.name = filename;
+      this.lastModified = (options && options.lastModified) || Date.now();
+    }
+  };
+}
+
 import { RemixBrowser } from '@remix-run/react';
 import { startTransition } from 'react';
 import { hydrateRoot } from 'react-dom/client';
